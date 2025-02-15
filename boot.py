ababaea9074c01d64c8533175176deea68ab0f5c
Will Matthews <william.matthews@podium.com>
2025-02-15 16:59:23 -0500

    removed space

--- boot.py
+++ boot.py
@@ -1,11 +1,9 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
 #import esp
 #esp.osdebug(None)
-import os, machine
+import os, machine, network
 #os.dupterm(None, 1) # disable REPL on UART(0)
 import gc
 import webrepl
 webrepl.start()
 gc.collect()
- 
- 
