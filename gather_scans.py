428bce628bbd34474e550bd4791aee81ec11631f
Will Matthews <william.matthews@podium.com>
2025-02-16 14:08:23 -0500

    updating gather scans

--- gather_scans.py
+++ gather_scans.py
@@ -1,24 +1,21 @@
 import network
 import ustruct
-import csv
+import time
+
+log_file = 'scan_log.csv'
 
 # Function to log the scan results to a CSV file
 def log_scan_result(mac_address, channel, rssi):
-    with open('scan_log.csv', 'a', newline='') as csvfile:
-        fieldnames = ['MAC Address', 'Channel', 'RSSI']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-        # Write the header if the file is empty
-        csvfile.seek(0, 2)  # Move to the end of the file
-        if csvfile.tell() == 0:
-            writer.writeheader()
-
-        writer.writerow({'MAC Address': mac_address, 'Channel': channel, 'RSSI': rssi})
+    with open(log_file, 'a') as csvfile:
+        writer = csvfile.write(f"{mac_address}, {channel}, {rssi}\n")
 
 # Function to perform the scan
 def scan_network():
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
+
+    # get the current time
+    current_time = time.time()
 
     # Perform WiFi scan
     networks = wlan.scan()
@@ -30,7 +27,7 @@ def scan_network():
         rssi = network[3]  # RSSI value is the 4th element
 
         # Log to CSV
-        log_scan_result(mac_address, channel, rssi)
+        log_scan_result(current_time, mac_address, channel, rssi)
 
 # Run the network scan and log the results
 scan_network()
