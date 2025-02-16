ef79b7d395bbac113ed1799e06f8df004210eb6b
Will Matthews <william.matthews@podium.com>
2025-02-15 20:13:25 -0500

    update code

--- gather_scans.py
+++ gather_scans.py
@@ -1,22 +1,36 @@
 import network
-import time
+import ustruct
+import csv
 
-ap = network.WLAN(network.AP_IF)
-ap.active(True)
-ap.config(essid='ESP8266_Proximeter', password='IDM06-18-2010')
+# Function to log the scan results to a CSV file
+def log_scan_result(mac_address, channel, rssi):
+    with open('scan_log.csv', 'a', newline='') as csvfile:
+        fieldnames = ['MAC Address', 'Channel', 'RSSI']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-print("AP Mode Active. Connect to ESP8266_Proximeter")
-print('IP Address: ', ap.ifconfig()[0])
+        # Write the header if the file is empty
+        csvfile.seek(0, 2)  # Move to the end of the file
+        if csvfile.tell() == 0:
+            writer.writeheader()
 
-sta = network.WLAN(network.STA_IF)
-sta.active(True)
+        writer.writerow({'MAC Address': mac_address, 'Channel': channel, 'RSSI': rssi})
 
-def scan_wifi():
-    networks = sta.scan()
-    for net in networks:
-        ssid, mac, channel, rssi, authmode, hidden = net
-        print(f"MAC: {mac}, RSSI: {rssi}, Channel: {channel}")
+# Function to perform the scan
+def scan_network():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
 
-while True:
-    scan_wifi()
-    time.sleep(5)
+    # Perform WiFi scan
+    networks = wlan.scan()
+
+    # Log each found network to the CSV
+    for network in networks:
+        mac_address = ':'.join(['{:02X}'.format(byte) for byte in network[1]])  # Convert the byte array to MAC address
+        channel = network[2]
+        rssi = network[3]  # RSSI value is the 4th element
+
+        # Log to CSV
+        log_scan_result(mac_address, channel, rssi)
+
+# Run the network scan and log the results
+scan_network()
